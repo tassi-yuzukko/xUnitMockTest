5395a8a1edca445551fff6dac19fb361561eee7f
田島 圭 <13041@kumahira.jp>
2017-07-28 18:33:49 +0900

    プロジェクト ファイルを追加します。

---
+++ xUnitMockTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xUnitMockTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
